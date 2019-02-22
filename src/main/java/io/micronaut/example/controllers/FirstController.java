2711fb764f4c981487b96040808e56b4728318dc
diego <diego.a.leon24@gmail.com>
2019-02-22 10:11:27 -0500

    cambios en controller

--- src/main/java/io/micronaut/example/controllers/FirstController.java
+++ src/main/java/io/micronaut/example/controllers/FirstController.java
@@ -3,15 +3,16 @@ package io.micronaut.example.controllers;
 import io.micronaut.example.entities.Response;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.reactivex.Single;
 
 @Controller("/firstpoint")
-public class firstController {
+public class FirstController {
 
     @Get("/{name}")
-    public Response Hello(String name) {
+    public Single<Response> Hello(String name) {
         Response response = new Response();
         response.setMessage(name + " Welcome to First Point");
-        return response;
+        return Single.just(response);
     }
 
 }
